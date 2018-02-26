384c86221a23677a454235c4c1580430d26bc1c7
CM Student <cmstudent@UHWO-ACM-C-07.local>
2018-02-26 10:29:25 -1000

    Set up for datasource methods

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+//Define an array variable using an array literal with 5 items of type string
+    
+    var studentsArray = ["Clark", "Jaquawn", "Scott", "David", "Michael"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return studentsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell {
+        //set up cell to display items in studentArray
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        let text = studentsArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+            
+            
 }
-
