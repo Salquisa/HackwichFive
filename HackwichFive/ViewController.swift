246128b768a111083de733360d1a30f3ee74f681
CM Student <cmstudent@UHWO-ACM-C-07.local>
2018-03-19 10:58:40 -1000

    Finished Hackwich Seven

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -26,13 +26,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Do any additional setup after loading the view, typically from a nib.
         //set VC as deglate
         tableView.dataSource = self
+        tableView.delegate = self
         
         
         //Hackwich Seven, part 6
         let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
         let dict = NSDictionary(contentsOfFile: path!)
         
-        restaurantImageData = dict!.object(forKey: "restaurantImages") as! [String]
+        restaurantImageData = dict!.object(forKey:"restaurantimages") as! [String]
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -58,4 +59,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath as IndexPath, animated: true)
     }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "mySegue"
+        {
+            let s1 = segue.destination as! detailViewController
+            let imageIndex = tableView.indexPathForSelectedRow?.row
+            s1.imagePass = restaurantImageData[imageIndex!]
+        }
+    }
+
 }
+
+
